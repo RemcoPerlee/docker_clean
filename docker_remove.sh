1f8217f812a257d5c85882793f38902a90ee59b1
Remco Perlee <remco.perlee@gmail.com>
2019-03-27 20:15:02 +0100

    Kubernetes 1.14 removed docker

--- docker_remove.sh
+++ docker_remove.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-microk8s.docker rmi -f $(microk8s.docker images | grep "<none>" | awk "{print \$3}")
+docker rmi -f $(microk8s.docker images | grep "<none>" | awk "{print \$3}")
 
 # always OK
 exit 0
